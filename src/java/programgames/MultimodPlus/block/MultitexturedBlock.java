bf868a51759d357e600f23ee2e1d228c03b6783f
julien <julgamer1@gmail.com>
2018-02-18 17:23:26 +0100

    BlockRotating: OK

--- src/java/programgames/MultimodPlus/block/MultitexturedBlock.java
+++ src/java/programgames/MultimodPlus/block/MultitexturedBlock.java
@@ -10,47 +10,89 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import programgames.MultimodPlus.Main;
+import scala.reflect.internal.Trees.CaseDef;
 
 public class MultitexturedBlock extends Block {
 
 	public IIcon[] icons = new IIcon[6];
 	
+	
+
 	@Override
 	public void registerBlockIcons(IIconRegister reg) {
+
+
+		for (int i = 0; i < 6; i ++) {
+			this.icons[i] = reg.registerIcon(this.textureName + i);
 		
-	
-		 for (int i = 0; i < 6; i ++) {
-		        this.icons[i] = reg.registerIcon(this.textureName + i);
-		    }
-	
-		 }
+			
+		}
+		
+
+	}
 
 	protected MultitexturedBlock(String unlocalizedName, Material material) 
-    { 
-		
-        super(material);
-       
-        this.setBlockName(unlocalizedName);
-        this.setBlockTextureName(Main.MODID + ":" + unlocalizedName);
-    
-        this.setCreativeTab(CreativeTabs.tabBlock);
-        this.setHardness(2.0F);
-        this.setResistance(6.0F);
-        this.setStepSound(soundTypeGravel);
-    }
+	{ 
+
+		super(material);
+
+		this.setBlockName(unlocalizedName);
+		this.setBlockTextureName(Main.MODID + ":" + unlocalizedName);
+
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setHardness(2.0F);
+		this.setResistance(6.0F);
+		this.setStepSound(soundTypeGravel);
+	}
 
 	@Override
-	public IIcon getIcon(int side, int meta) {
-		if((side == 3 && meta == 0) || (side == 4 && meta == 1) || (side == 2 && meta== 2) || (side == 5 && meta== 3))
+	public IIcon getIcon(int side, int metadata) {
+		
+		if(metadata == 1)
 		{
-		return this.icons[meta];
+			if(side == 4)
+				return this.icons[3];
+			if(side == 3 )
+				return this.icons[5];
+			if(side == 5)
+				return this.icons[2];
+			if(side == 2)
+				return this.icons[4];
+			if(side == 1 || side == 0)
+				return this.icons[side];
 		}
-		return this.icons[meta];
-	    
+		if(metadata == 2)
+		{
+			if(side == 4)
+				return this.icons[5];
+			if(side == 3 )
+				return this.icons[2];
+			if(side == 5)
+				return this.icons[4];
+			if(side == 2)
+				return this.icons[3];
+			if(side == 1 || side == 0)
+				return this.icons[side];
+		}
+		if(metadata == 3)
+		{
+			if(side == 4)
+				return this.icons[5];
+			if(side == 3 )
+				return this.icons[4];
+			if(side == 5)
+				return this.icons[3];
+			if(side == 2)
+				return this.icons[5];
+			if(side == 1 || side == 0)
+				return this.icons[side];
+		}
+		return this.icons[side];
+
 	}
 	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase living, ItemStack stack)
 	{
-	int direction = MathHelper.floor_double((double)(living.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
-	world.setBlockMetadataWithNotify(x, y, z, direction, 2);
+		int direction = MathHelper.floor_double((double)(living.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+		world.setBlockMetadataWithNotify(x, y, z, direction, 2);
 	}
 }
